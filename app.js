79c68fe2a0475ce66ba718329c94747732634db4
Samanta na Seplan <samantasa087@gmail.com>
2025-09-19 09:58:42 -0300

    Criando repositório

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p');
+
+titulo.innerHTML = "Jogo do número Secreto";
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+
+function verificarChute() {
+    
+}
