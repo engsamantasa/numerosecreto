8d62835fe8ab1572547b2bf4cfaeb5f59d653a0a
Samanta na Seplan <samantasa087@gmail.com>
2025-09-22 11:13:01 -0300

    Aula  4 finalizada

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-let NumeroSecreto = gerarNumeroSecreto()
-let tentativas = 1
+let numeroMaximo = 10;
+let listaNumeroSecreto = [];
+let numeroSecreto = gerarNumeroSecreto();
+let tentativas = 1;
+
 
 function exibirTextoNoSite(tag,texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-}
+};
+
 
 function exibirMensagemInicial(){
     exibirTextoNoSite('h1' , 'Jogo do Número Secreto')
-    exibirTextoNoSite('p', 'Escolha um número entre 1 e 10')
-}
+    exibirTextoNoSite('p', `Escolha um número entre 1 e ${numeroMaximo}`)
+};
+
 
 exibirMensagemInicial();
 
@@ -17,19 +22,20 @@ exibirMensagemInicial();
 function verificarChute() {
     let chute = document.querySelector('input').value
 
-    if(chute == NumeroSecreto){
+    if(chute == numeroSecreto){
         exibirTextoNoSite('h1' , 'Você acertou!');
         let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `O número secreto realmente é ${chute}! Você descobriu o número secreto com ${tentativas} ${palavraTentativas}.`
         exibirTextoNoSite('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled')
         limparCampo();
+        document.getElementById('chutando').setAttribute('disabled',true);
 
     }else{
 
-        if (chute > NumeroSecreto ){
+        if (chute > numeroSecreto ){
            exibirTextoNoSite('p', `O número secreto é menor que ${chute} .`);     
-        } if (chute < NumeroSecreto ){
+        } if (chute < numeroSecreto ){
             exibirTextoNoSite('p', `O número secreto é maior que ${chute} .`);   
         }
         tentativas ++;
@@ -41,20 +47,34 @@ function verificarChute() {
  
 
 function reiniciarJogo(){
-    NumeroSecreto = gerarNumeroSecreto();
+    numeroSecreto = gerarNumeroSecreto();
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled',true);
+    document.getElementById('chutando').removeAttribute('disabled');
+};
 
-
-}
 
 function limparCampo(){
     chute = document.querySelector('input');
     chute.value = '';
-}
+};
+
 
 function gerarNumeroSecreto(){
-    return parseInt( Math.random() * 10 + 1 )
-}
+    let numeroEscolhido =  parseInt( Math.random() * numeroMaximo + 1 );
+    let quantidadeElementosNaLista = listaNumeroSecreto.length;
+
+    if (quantidadeElementosNaLista == numeroMaximo){
+        listaNumeroSecreto = [];
+    }
+
+    if (listaNumeroSecreto.includes(numeroEscolhido)){
+        return gerarNumeroSecreto();
+    } else {
+        listaNumeroSecreto.push(numeroEscolhido);
+        console.log(listaNumeroSecreto)
+        return numeroEscolhido
+    }
+};
